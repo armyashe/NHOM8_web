dfc97430a96aa620fe57110d070184b642521724
armyashe <147798829+armyashe@users.noreply.github.com>
2023-10-20 10:18:48 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var index=1;
+        changeImge = function()
+        {
+            var imgs = ["./IMG/background.jpg","./IMG/background_1.jpg","./IMG/background_2.jpg","./IMG/background_3.jpg"];
+            switch (index) {
+                case 1:
+                    document.getElementById('img').style.backgroundImage = "url(IMG/background_1.jpg)";
+                    break;
+                
+                case 2:
+                    document.getElementById('img').style.backgroundImage = "url(IMG/background_2.jpg)";
+                    break;
+                
+                case 3:
+                    document.getElementById('img').style.backgroundImage = "url(IMG/background_3.jpg)";
+                    break;
+                
+                case 0:
+                    document.getElementById('img').style.backgroundImage = "url(IMG/background.jpg)";
+                    break;
+            }
+            index++;
+            if(index==4) index = 0;
+        }
+        setInterval(changeImge,2500);
