5df6a78c80cff9530c6e5c1bae5c785224b5aaea
MY HUONG <147798829+armyashe@users.noreply.github.com>
2023-10-27 09:58:09 +0700

    update run

--- NHOM8_web/main.js
+++ NHOM8_web/main.js
@@ -1,6 +1,7 @@
 var index=1;
         changeImge = function()
         {
+            document.getElementById('img').style.backgroundImage = "url(IMG/background.jpg)";
             var imgs = ["./IMG/background.jpg","./IMG/background_1.jpg","./IMG/background_2.jpg","./IMG/background_3.jpg"];
             switch (index) {
                 case 1:
